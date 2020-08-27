124aba56f8f0d425a3f3cb2cc8daf262cc5081ef
Rahul Arora <rahularorar95@gmail.com>
2020-08-27 19:30:14 +0530

    header bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from './Header';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      {/* Header */}
+      <Header />
+      {/* Cards */}
+
+      {/* Buttons Below Cards */}
+
+
+      {/* Chat Screen */}
+
+      {/* Individual chat screen */}
+
     </div>
   );
 }
